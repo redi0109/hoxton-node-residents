17d17bdc5506108f0ad688ca7fdfd32f104937d4
Demir Boraj <demirboraj97@gmail.com>
2022-09-05 01:14:34 +0200

    1

--- index.ts
+++ index.ts
@@ -2,3 +2,20 @@ import express from "express";
 import cors from "cors";
 import { housesData, residentsData } from "./data";
 
+let houses = housesData;
+let residents = residentsData;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('REDIIII')
+})
+
+
+app.listen(port, () => {
+    console.log(`Go to: http://localhost:${port}`)
+})
